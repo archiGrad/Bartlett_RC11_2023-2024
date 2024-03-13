c173921d0d6eb1ca818f41d551051a480167fce3
putteneersjoris <putteneersjoris@gmail.com>
2024-03-13 10:40:34 +0100

    update bartlett session 5

--- 5_dict_visualizer/extract_and_download.py
+++ 5_dict_visualizer/extract_and_download.py
@@ -9,23 +9,21 @@ imagepath.mkdir(exist_ok=True) #check if "./images" exist. if not, make this dir
 
 
 # download the sora youtube video
-url ="https://www.youtube.com/watch?v=EhUsQvoAeAY"
+url ="https://www.youtube.com/watch?v=U2Qp5pL3ovA"
 
 yt = YouTube(url) #store the youtube object with url as input.
 
-#https://pytube.io/en/latest/user/streams.html 
-# we want a low framerate by huigh resolution mp4 version. 137 seesm good
 
-video = yt.streams.get_by_itag(137)
+video = yt.streams.get_by_itag(160)
 video.download()
 
 frame_no = 0
 
-cap = cv2.VideoCapture("Introducing Sora — OpenAI’s text-to-video model.mp4")
+cap = cv2.VideoCapture("Dune Part Two  Official Trailer 3.mp4")
 while (cap.isOpened()):
 	ret, frame = cap.read()
-	if(frame_no % 10 == 0):
+	if(frame_no % 1 == 0):
 		name =  f"frame_{frame_no}.jpg"
 		target = os.path.join(imagepath,name)
 		cv2.imwrite(target, frame)
-	frame_no +=1		
+	frame_no +=1	
